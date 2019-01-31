4d30ea7f45f3c64cfaf0924d5d3edf79b792ea36
Emmanuel Franquiz <emmanueljfranquizs@gmail.com>
2019-01-31 00:44:37 -0400

    initial and template

---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,57 @@
+                <aside id="sidebar-left" class="sidebar-left">
+                
+                    <div class="sidebar-header">
+                        <div class="sidebar-title">
+                            Navigation
+                        </div>
+                        <div class="sidebar-toggle hidden-xs" data-toggle-class="sidebar-left-collapsed" data-target="html" data-fire-event="sidebar-left-toggle">
+                            <i class="fa fa-bars" aria-label="Toggle sidebar"></i>
+                        </div>
+                    </div>
+                
+                    <div class="nano">
+                        <div class="nano-content">
+                            <nav id="menu" class="nav-main" role="navigation">
+                                <ul class="nav nav-main">
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-home" aria-hidden="true"></i>
+                                            <span>Agence</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa  fa-check-square-o" aria-hidden="true"></i>
+                                            <span>Projectos</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa  fa-edit" aria-hidden="true"></i>
+                                            <span>Administrativo</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa  fa-group" aria-hidden="true"></i>
+                                            <span>Comercial</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa  fa-list-alt" aria-hidden="true"></i>
+                                            <span>Financeiro</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-user" aria-hidden="true"></i>
+                                            <span>Usuario</span>
+                                        </a>
+                                    </li>      
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                
+                </aside>
